a5e55422b74b4b57f4c90a9fd6762ae80c2506de
Vika Ratomskaya <ratom_v@mail.ru>
2020-11-13 01:32:30 +0300

    Added Matrix Multiplication Fixed bug with get_value

--- TestAssignment.py
+++ TestAssignment.py
@@ -1,9 +1,5 @@
-
 class Matrix:
-    class ValueRowError(Exception):
-        pass
-
-    class ValueColError(Exception):
+    class SizeMisMatch(Exception):
         pass
 
     def __init__(self, row, col):
@@ -18,30 +14,37 @@ class Matrix:
         return self._col
     
     def get_value(self, r, c):
-        value = r*self._col + c
-        return value
+        return self._value[r*self._col + c]
 
     def set_value(self, r, c, value):
         self._value[r*self._col + c] = value
     
     def __add__(self, matrix):
-        if self._row != matrix._row:
-            raise Matrix.ValueRowError()
-        if self._col != matrix._col:
-            raise Matrix.ValueColError()
+        if self._row != matrix._row or self._col != matrix._col:
+            raise Matrix.SizeMisMatch()
         result = Matrix(self._row, self._col)
         for i in range(len(self._value)):
             result._value[i] = self._value[i] + matrix._value[i]
         return result
     
     def __sub__(self, matrix):
-        if self._row != matrix._row:
-            raise Matrix.ValueRowError()
-        if self._col != matrix._col:
-            raise Matrix.ValueColError()
+        if self._row != matrix._row or self._col != matrix._col:
+            raise Matrix.SizeMisMatch()
         result = Matrix(self._row, self._col)
         for i in range(len(self._value)):
             result._value[i] = self._value[i] - matrix._value[i]
+        return result
+    
+    def __mul__(self, matrix):
+        if self._col != matrix._row:
+            raise Matrix.SizeMisMatch()
+        result = Matrix(self._row, matrix._col)
+        for i in range(result._row):
+            for j in range(result._col):
+                value = 0
+                for v in range(self._col):
+                    value += self.get_value(i, v) * matrix.get_value(v, j)
+                result.set_value(i, j, value)
         return result
     
     def __str__(self):
@@ -56,8 +59,10 @@ for i in range(B.row_value()):
     for j in range(B.col_value()):
         B.set_value(i, j, i+j)
 C = A + B
-D = A - A
+D = C + A - C
+G = A * B
 print(f'A = {A}')
 print(f'B = {B}')
 print(f'C = {C}')
 print(f'D = {D}')
+print(f'G = {G}')
