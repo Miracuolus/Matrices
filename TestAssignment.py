5be7095a39d1e199eeb5b2a801e929873456c3bd
Vika Ratomskaya <ratom_v@mail.ru>
2020-11-14 23:55:18 +0300

    Added Exceptions for requirements

--- TestAssignment.py
+++ TestAssignment.py
@@ -1,6 +1,15 @@
+class IllegalInputException(Exception):
+        def __init__(self, message):
+            self.message = message
 class Matrix:
-    class SizeMissMatch(Exception):
-        pass
+    class SizeMissMatchException(Exception):
+        def __init__(self, message):
+            self.message = message
+    
+    class IllegalArgumentException(Exception):
+        def __init__(self, message):
+            self.message = message
+
 
     def __init__(self, row, col):
         self._row = row
@@ -21,7 +30,7 @@ class Matrix:
     
     def __add__(self, matrix):
         if self._row != matrix._row or self._col != matrix._col:
-            raise Matrix.SizeMissMatch()
+            raise Matrix.SizeMissMatchException("Can't perform addition")
         result = Matrix(self._row, self._col)
         for i in range(len(self._value)):
             result._value[i] = self._value[i] + matrix._value[i]
@@ -29,7 +38,7 @@ class Matrix:
     
     def __sub__(self, matrix):
         if self._row != matrix._row or self._col != matrix._col:
-            raise Matrix.SizeMissMatch()
+            raise Matrix.SizeMissMatchException("Can't perform subtraction")
         result = Matrix(self._row, self._col)
         for i in range(len(self._value)):
             result._value[i] = self._value[i] - matrix._value[i]
@@ -37,7 +46,7 @@ class Matrix:
     
     def __mul__(self, matrix):
         if self._col != matrix._row:
-            raise Matrix.SizeMissMatch()
+            raise Matrix.SizeMissMatchException("Can't perform multiplication")
         result = Matrix(self._row, matrix._col)
         for i in range(result._row):
             for j in range(result._col):
@@ -55,11 +64,14 @@ class Matrix:
         for j in rows:
             l = len(j)
             if len(rows[0]) != l:
-                raise Matrix.SizeMissMatch()
+                raise Matrix.SizeMissMatchException("Different number of items in rows")
         m = Matrix(len(rows), len(rows[0]))
         for i in range(len(rows)):
             for j in range(len(rows[i])):
-                m.set_value(i, j, int(rows[i][j]))
+                try:
+                    m.set_value(i, j, int(rows[i][j]))
+                except ValueError:
+                    raise Matrix.IllegalArgumentException("Can't read matrix")
         return m
     
     def __str__(self):
@@ -80,10 +92,19 @@ try:
     while True:
         line = input()
         if not line or not line.strip():
-            break
-        name, value = line.split('=')
-        matrix_dict[name] = Matrix.fromstring(value)
+            if len(matrix_dict) == 0:
+                raise IllegalInputException('Incorrect input pattern')
+            else:
+                break
+        name, *value = line.split('=')
+        if len(value) != 1:
+            raise IllegalInputException('Incorrect input matrix')
+        if not name.isupper() or len(name) != 1:
+            raise Matrix.IllegalArgumentException('Wrong name of matrix')
+        matrix_dict[name] = Matrix.fromstring(*value)
     cond = input()
+    if not cond or not cond.strip():
+        raise IllegalInputException('Incorrect input pattern')
     result = exec('print('+cond+')', matrix_dict)
-except ValueError:
-    print('*Ошибка! Необходимо вводить данные в числовых значениях')
+except (Matrix.SizeMissMatchException, Matrix.IllegalArgumentException, IllegalInputException) as exception:
+    print('Exception caughtg: ' + type(exception).__name__ +'. ' + exception.message)
